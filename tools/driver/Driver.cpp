ec832494fac4c22c8d6de161285bddf7346cde4f
Kyohyun Lee <kakunge3@naver.com>
2024-01-12 02:35:14 +0900

    Update Driver.cpp

--- tools/driver/Driver.cpp
+++ tools/driver/Driver.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc_, const char **argv_) {
 	llvm::InitLLVM X(argc_, argv_);
-	llvm::outs() << "Hello, I am Tinylang" << tinylang::getTinylangVersion()
+	llvm::outs() << "Hello, I am Tinylang " << tinylang::getTinylangVersion()
 		<< "\n";
 }
 
